6d72dbbb2870481cd52a668029b094ded8e8949e
Ahmad Qarshi <ahmad.qarshi@outlook.com>
2019-11-01 15:06:44 +0500

    New test entities introduced

---
+++ SlingshotApp/Applicant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlingshotApp
+{
+    class Applicant
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime DOB { get; private set; }
+        public UserRoles Roles { get; private set; }
+
+        public Applicant(string firstName, string lastName, DateTime dob, UserRoles roles)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DOB = dob;
+            Roles = roles;
+        }
+
+    }
+}
--- SlingshotApp/Program.cs
+++ SlingshotApp/Program.cs
@@ -6,6 +6,8 @@ namespace SlingshotApp
     {
         static void Main(string[] args)
         {
+            var applicant = new Applicant("Adam", "smith", DateTime.Now, new UserRoles());
+            var admin = new Admin("Shaun", "Tait", DateTime.Now, new UserRoles());
 
         }
     }
