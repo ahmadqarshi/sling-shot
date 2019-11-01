f3a19f1ee9e79d427bec692c3a93acefc553a4ec
Ahmad Qarshi <ahmad.qarshi@outlook.com>
2019-11-01 15:57:47 +0500

    Removed Applicant reference in Main from the from-pra branch

--- SlingshotApp/Program.cs
+++ SlingshotApp/Program.cs
@@ -6,7 +6,7 @@ namespace SlingshotApp
     {
         static void Main(string[] args)
         {
-            var applicant = new Applicant("Adam", "smith", DateTime.Now, new UserRoles());
+            
             var admin = new Admin("Shaun", "Tait", DateTime.Now, new UserRoles());
 
         }
